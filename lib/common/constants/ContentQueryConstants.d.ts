d617411157cd2c9afb2b2277d431e48410b3e0bb
shay silberstein <shay7566@gmail.com>
2019-06-10 21:11:15 +0300

    upload

---
+++ lib/common/constants/ContentQueryConstants.d.ts
@@ -0,0 +1,19 @@
+export declare class ContentQueryConstants {
+    /**************************************************************
+     * WebPart Properties
+     **************************************************************/
+    static readonly propertySiteUrl: string;
+    static readonly propertyWebUrl: string;
+    static readonly propertyListId: string;
+    static readonly propertyOrderBy: string;
+    static readonly propertOrderByDirection: string;
+    static readonly propertyLimitEnabled: string;
+    static readonly propertyItemLimit: string;
+    static readonly propertyRecursiveEnabled: string;
+    static readonly propertyFilters: string;
+    static readonly propertyViewFields: string;
+    static readonly propertyTemplateText: string;
+    static readonly propertyTemplateUrl: string;
+    static readonly propertyhasDefaultTemplateBeenUpdated: string;
+    static readonly propertyExternalScripts: string;
+}
